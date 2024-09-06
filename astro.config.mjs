cf8ba7c3532a4ea92ce73f359abf4fe67e6d639a
Pedro Carreno <34664891+Pkcarreno@users.noreply.github.com>
2024-09-06 19:48:53 -0400

    chore: improve astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,19 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   site: "https://dresnerca.com",
-  integrations: [tailwind(), sitemap()],
+  build: {
+    inlineStylesheets: "always",
+  },
+  compressHTML: true,
+  prefetch: true,
+  devToolbar: {
+    enabled: false,
+  },
+  integrations: [
+    tailwind({
+      applyBaseStyles: false,
+      nesting: true,
+    }),
+    sitemap(),
+  ],
 });
