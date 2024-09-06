ca763f4824f988ab31cfd47a4425028468a1a262
Pedro Carreno <34664891+Pkcarreno@users.noreply.github.com>
2024-09-06 18:52:01 -0400

    chore: add eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,49 @@
+import eslint from "@eslint/js";
+import eslintPluginAstro from "eslint-plugin-astro";
+import tailwind from "eslint-plugin-tailwindcss";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(
+  {
+    ignores: ["node_modules/*", "dist/*", ".astro"],
+  },
+  ...eslintPluginAstro.configs["flat/recommended"],
+  {
+    extends: [eslint.configs.recommended, ...tseslint.configs.recommended],
+    files: ["src/**/*.{ts,tsx,js}", "./*.{ts,tsx,js}"],
+    languageOptions: {
+      parserOptions: {
+        project: ["./tsconfig.json"],
+        ecmaFeatures: {
+          jsx: true,
+        },
+        ecmaVersion: 11,
+        sourceType: "module",
+      },
+    },
+    rules: {
+      "@typescript-eslint/comma-dangle": "off",
+      "@typescript-eslint/consistent-type-imports": "error",
+      "@typescript-eslint/no-unused-vars": "off",
+      "@typescript-eslint/triple-slash-reference": "off",
+    },
+  },
+  ...tailwind.configs["flat/recommended"],
+  {
+    settings: {
+      tailwindcss: {
+        callees: ["cn", "cva"],
+        config: "tailwind.config.cjs",
+      },
+    },
+    rules: {
+      "tailwindcss/classnames-order": [
+        "warn",
+        {
+          officialSorting: true,
+        },
+      ],
+      "tailwindcss/no-custom-classname": "off",
+    },
+  }
+);
