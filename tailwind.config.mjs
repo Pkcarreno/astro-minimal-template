ff98d6540acb6d756b20bd601554ef0d32020614
Pedro Carreno <34664891+Pkcarreno@users.noreply.github.com>
2024-09-06 21:56:41 -0400

    chore: implement tailwind

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,21 @@
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+      },
+      fontFamily: {},
+    },
   },
   plugins: [],
 };
