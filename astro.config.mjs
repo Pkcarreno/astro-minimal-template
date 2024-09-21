cf70cd26c5d43e8b4e78459c27a2af21a442ba64
Pedro Carreno <34664891+Pkcarreno@users.noreply.github.com>
2024-09-21 00:00:40 -0400

    chore: restore some astro defaults

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,15 +5,11 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://dresnerca.com",
   build: {
     inlineStylesheets: "always",
   },
   compressHTML: true,
   prefetch: true,
-  devToolbar: {
-    enabled: false,
-  },
   integrations: [
     tailwind({
       applyBaseStyles: false,
