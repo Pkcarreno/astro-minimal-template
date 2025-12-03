6e8d4974b2415b82090a71dec8329618dac7487d
Pedro Carreno <34664891+Pkcarreno@users.noreply.github.com>
2025-12-03 17:46:40 -0400

    feat(dx): migrate linter to biome

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,11 +1,5 @@
 export default {
-  "**/*.{astro,html,js,jsx,svelte,ts,tsx,vue}": (filenames) => [
-    `npx eslint --fix ${filenames
-      .map((filename) => `"${filename}"`)
-      .join(" ")}`,
-  ],
-  "**/*.(md|mdx|json)": (filenames) =>
-    `npx prettier --write ${filenames
-      .map((filename) => `"${filename}"`)
-      .join(" ")}`,
-};
+	'**/*.{astro,html,js,jsx,svelte,ts,tsx,vue,md,mdx,json}': filenames => [
+		`npx @biomejs/biome check --write ${filenames.map(filename => `"${filename}"`).join(' ')}`
+	]
+}
