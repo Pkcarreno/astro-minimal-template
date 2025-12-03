ba3eb3c94c243a6510b24b874c2ecd259aaf8957
Pedro Carreno <34664891+Pkcarreno@users.noreply.github.com>
2025-12-03 18:25:54 -0400

    feat(dx): tweak structure

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,14 +5,14 @@ import { defineConfig } from 'astro/config'
 import favicons from 'astro-favicons'
 import robots from 'astro-robots'
 
-import { config } from './src/config'
-
 // For type-safe environment variables, check this
 // https://docs.astro.build/en/guides/environment-variables/#type-safe-environment-variables
 
+const site = 'https://example.org'
+
 // https://astro.build/config
 export default defineConfig({
-	site: config.site,
+	site,
 	env: {
 		schema: {
 			// API_URL: envField.string({
