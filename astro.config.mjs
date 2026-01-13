4e44c5f6907415e28f0628349e639cee60036227
Pedro Carreno <34664891+Pkcarreno@users.noreply.github.com>
2026-01-13 14:34:47 -0400

    feat: tweak dx tools

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,39 +1,33 @@
 // @ts-check
-import sitemap from '@astrojs/sitemap'
-import tailwindcss from '@tailwindcss/vite'
-import { defineConfig } from 'astro/config'
-import favicons from 'astro-favicons'
-import robots from 'astro-robots'
+import sitemap from "@astrojs/sitemap";
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "astro/config";
+import favicons from "astro-favicons";
+import robots from "astro-robots";
 
-// For type-safe environment variables, check this
-// https://docs.astro.build/en/guides/environment-variables/#type-safe-environment-variables
-
-const site = 'https://example.org'
+const site = "https://example.org";
 
 // https://astro.build/config
 export default defineConfig({
-	site,
-	env: {
-		schema: {
-			// API_URL: envField.string({
-			//   context: "client",
-			//   access: "public",
-			// }),
-			// PORT: envField.number({
-			//   context: "server",
-			//   access: "public",
-			//   default: 4321,
-			// }),
-			// API_SECRET: envField.string({ context: "server", access: "secret" }),
-		}
-	},
-	build: {
-		inlineStylesheets: 'always'
-	},
-	compressHTML: true,
-	prefetch: true,
-	integrations: [favicons(), robots(), sitemap()],
-	vite: {
-		plugins: [tailwindcss()]
-	}
-})
+  site,
+  env: {
+    schema: {
+      // API_URL: envField.string({
+      //   context: "client",
+      //   access: "public",
+      // }),
+      // PORT: envField.number({
+      //   context: "server",
+      //   access: "public",
+      //   default: 4321,
+      // }),
+      // API_SECRET: envField.string({ context: "server", access: "secret" }),
+    },
+  },
+  compressHTML: true,
+  prefetch: true,
+  integrations: [favicons(), robots(), sitemap()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
